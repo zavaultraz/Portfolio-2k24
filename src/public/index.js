5f81aedf458cbfa1e49bd1b7160ca47775322260
zavaultraz <zavazaneta@gmail.com>
2024-02-19 13:52:10 +0700

    mantap

---
+++ src/public/index.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Game Developer', 'Student'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
